c5ccf52590cadedaf8c4054feb48ec59226160f8
jerrydog <qinhuajun@outlook.com>
2017-07-05 11:34:17 +0800

    add network submodule

---
+++ network/network.go
@@ -0,0 +1,51 @@
+// network子包提供网络相关的一些小函数
+package network
+
+import (
+	"errors"
+	"net"
+	"strings"
+)
+
+// 获取本地IP地址，除loopback地址(127.0.0.1)
+func GetLocalIpAddrs() (ipaddrs []string, err error) {
+	var (
+		addrs []net.Addr
+		addr  []net.Addr
+	)
+
+	// 获取所有网络接口
+	i, err := net.Interfaces()
+	if err != nil {
+		return
+	}
+
+	// 循环每个网络接口,如果一个接口获取addrs失败，不返回，而是继续
+	// 这样可以保证获取可用addrs
+	for _, iface := range i {
+		if strings.Contains(strings.ToLower(iface.Flags.String()), "loopback") {
+			continue
+		}
+		addr, err = iface.Addrs()
+		if err != nil {
+			continue
+		}
+		addrs = append(addrs, addr...)
+	}
+
+	if len(addrs) == 0 {
+		err = errors.New("can not get any addrs")
+	}
+
+	ipaddrs = make([]string, len(addrs))
+	for _, addr := range addrs {
+		inet, ok := addr.(*net.IPNet)
+		if ok {
+			ip := inet.IP.To4()
+			if ip != nil {
+				ipaddrs = append(ipaddrs, ip.String())
+			}
+		}
+	}
+	return
+}
---
+++ tools.go
@@ -0,0 +1,2 @@
+// tools包提供一些常用小的接口
+package tools
