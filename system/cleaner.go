5154fdc7b57413a4db4cf61a1c573f4c67d5d433
jerrydog <qinhuajun@outlook.com>
2017-07-05 17:57:53 +0800

    update exitcleaner

--- system/cleaner.go
+++ system/cleaner.go
@@ -1,5 +1,10 @@
 package system
 
+import (
+	"errors"
+
+	"golang.org/x/sync/syncmap"
+)
 
 // 定义清理器接口
 type ExitCleaner interface {
@@ -8,13 +13,30 @@ type ExitCleaner interface {
 
 // 定义清理器列表
 type CleanList struct {
-		
+	list *syncmap.Map
 }
 
 //
 func (p *CleanList) Add(ec ExitCleaner) error {
-
+	_, exist := p.list.Load(ec)
+	if exist {
+		return errors.New("the key already exists")
+	}
+	p.list.Store(ec, "")
+	return nil
 }
 
 //
-func
+func (p *CleanList) Run() {
+	p.list.Range(runClean)
+}
+
+//
+func runClean(key interface{}, value interface{}) bool {
+	newkey, ok := key.(ExitCleaner)
+	if !ok {
+		return true
+	}
+	newkey.ExitClean()
+	return true
+}
