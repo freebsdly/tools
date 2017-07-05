c738f158f38c4ae22ef3408926a8af6883ef64e1
jerrydog <qinhuajun@outlook.com>
2017-07-05 18:19:52 +0800

    modify cleaner package

--- system/cleaner/cleaner.go
+++ system/cleaner/cleaner.go
@@ -1,9 +1,18 @@
-package system
+// cleaner包,用于程序退出时,执行清理操作
+package cleaner
 
 import (
 	"errors"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"golang.org/x/sync/syncmap"
+)
+
+//
+var (
+	defaultList = new(CleanList)
 )
 
 // 定义清理器接口
@@ -31,7 +40,8 @@ func (p *CleanList) Run() {
 	p.list.Range(runClean)
 }
 
-//
+// map的range方法的回调函数
+// 用于运行ExitCleaner的Clean方法
 func runClean(key interface{}, value interface{}) bool {
 	newkey, ok := key.(ExitCleaner)
 	if !ok {
@@ -39,4 +49,28 @@ func runClean(key interface{}, value interface{}) bool {
 	}
 	newkey.ExitClean()
 	return true
+}
+
+//
+func AddCleaner(ec ExitCleaner) error {
+	return defaultList.Add(ec)
+}
+
+//
+func Exit(n int) {
+	//创建一个元素为信号量的通道
+	sigs := make(chan os.Signal, 1)
+
+	//监听系统信号量
+	signal.Notify(sigs,
+		syscall.SIGHUP,
+		syscall.SIGKILL,
+		syscall.SIGTERM,
+	)
+	go func(num int) {
+		<-sigs
+		defaultList.Run()
+		os.Exit(num)
+	}(n)
+
 }
